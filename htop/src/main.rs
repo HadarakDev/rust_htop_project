2e6ef171d9d0bd7eba978436b3370aea91c0fc20
Antoine Tavernier <tavernier.antoine@hotmail.fr>
2020-06-08 23:02:01 +0200

    run to step by step

--- htop/src/main.rs
+++ htop/src/main.rs
@@ -1,9 +1,12 @@
 extern crate sysinfo;
+
+use std::fmt;
+
 use cursive::views::Dialog;
 use cursive::Cursive;
 use cursive::views::LinearLayout;
 
-use sysinfo::{NetworkExt, ProcessExt, ProcessorExt, System, SystemExt};
+use sysinfo::{NetworkExt, ProcessExt, ProcessorExt, System, SystemExt, DiskExt};
 
 use std::{thread, time};
 
@@ -17,18 +20,18 @@ fn get_my_processes(system : &mut sysinfo::System) -> String {
         my_vec.push(format!("{:?}", process.memory()));
         my_vec.push(format!("{:?}", process.status()));
     }
-    let mut my_s = String::new();
+    let mut my_s = String::with_capacity(2048);
     my_s.push_str(&format!("{:^5}: {:^6}: {:^6}: {:^6}: {:^6}\n", "pid", "name", "cpu(%)", "memory(kb)",  "status"));
     for x in (0..my_vec.len()).step_by(5) {
-        my_s.push_str(&format!("{:^5}", &my_vec[x]).to_string());
+        my_s.push_str(&format!("{:^5}", &my_vec[x]));
         my_s.push_str(": ");
-        my_s.push_str(&format!("{:^6}", &my_vec[x + 1]).to_string());
+        my_s.push_str(&format!("{:^6}", &my_vec[x + 1]));
         my_s.push_str(": ");
-        my_s.push_str(&format!("{:^6}", &my_vec[x + 2])[0..6].to_string());
+        my_s.push_str(&format!("{:^6}", &my_vec[x + 2])[0..6]);
         my_s.push_str(": ");
-        my_s.push_str(&format!("{:^10}", &my_vec[x + 3]).to_string());
+        my_s.push_str(&format!("{:^10}", &my_vec[x + 3]));
         my_s.push_str(": ");
-        my_s.push_str(&format!("{:^6}", &my_vec[x + 4]).to_string());
+        my_s.push_str(&format!("{:^6}", &my_vec[x + 4]));
         my_s.push_str("\n");
     }
     return my_s;
@@ -43,9 +46,9 @@ fn get_my_cpu_usage(system : &mut sysinfo::System) -> String {
     for processor in system.get_processors() {
         my_vec.push(processor.get_cpu_usage());
     }
-    let mut my_s = String::new();
+    let mut my_s = String::with_capacity(2048);
     for x in (0..my_vec.len()).step_by(1) {
-        my_s.push_str(&format!("{:^3}", &my_vec[x]).to_string());
+        my_s.push_str(&format!("{:^3}", &my_vec[x]));
         my_s.push_str(" [");
         for i in (0..100).step_by(2) {
             if i < my_vec[x] as u8 {
@@ -56,6 +59,36 @@ fn get_my_cpu_usage(system : &mut sysinfo::System) -> String {
             }
         }
         my_s.push_str("]\n");
+    }
+    return my_s;
+}
+
+fn get_disk_type_string(disk : sysinfo::DiskType) -> String {
+    match disk {
+        sysinfo::DiskType::HDD => String::from("HDD"),
+        sysinfo::DiskType::SSD => String::from("SSD"),
+        _ => String::from("Unknown"),
+    }
+}
+
+
+fn get_my_disks(system : &mut sysinfo::System) -> String {
+    system.refresh_all();
+    let mut my_vec = Vec::new();
+    println!("MY disks");
+    for disk in system.get_disks() {
+        my_vec.push(disk.get_name().to_string_lossy().into_owned());
+        my_vec.push(get_disk_type_string(disk.get_type()));
+        // my_vec.push(disk.get_file_system());
+        // my_vec.push(disk.get_mount_point());
+        // my_vec.push(disk.get_total_space());
+        // my_vec.push(disk.get_available_space());
+    }
+    let mut my_s = String::with_capacity(2048);
+
+    for x in (0..my_vec.len()).step_by(2) {
+        my_s.push_str(&format!("{}", &my_vec[x]));
+        my_s.push_str("\n");
     }
     return my_s;
 }
@@ -80,15 +113,21 @@ fn main() {
 
     let mut siv = cursive::default();
     siv.add_global_callback('q', |s| s.quit());
-    siv.add_global_callback(' ', |s| my_loop(s));
-    thread::spawn(move || {
-        for i in (0..10000) {
-            thread::sleep(time::Duration::from_millis(100));
-        }
-    });
-    my_loop(&mut siv);
+    // siv.add_global_callback(' ', |s| my_loop(s));
 
-    siv.run();
+
+    while siv.is_running() {
+        my_loop(&mut siv);
+        siv.step();
+        siv.refresh();
+        thread::sleep(time::Duration::from_millis(500));
+    }
+    
+
+    // siv.run();
+    // let mut system = sysinfo::System::new_all();
+    // let s = get_my_disks(&mut system);
+    // println!("{}", s);
 
 
 
