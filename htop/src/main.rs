9c48f5bb33860b12f37b715d56d5176ae4ad59a7
Antoine Tavernier <tavernier.antoine@hotmail.fr>
2020-06-22 17:56:13 +0200

    [Memory] add memory + cleaning

--- htop/src/main.rs
+++ htop/src/main.rs
@@ -4,7 +4,7 @@ use cursive::views::Dialog;
 use cursive::Cursive;
 use cursive::views::LinearLayout;
 
-use sysinfo::{NetworkExt, ProcessExt, ProcessorExt, System, SystemExt, DiskExt};
+use sysinfo::{ProcessExt, ProcessorExt, SystemExt, DiskExt};
 
 use std::{thread, time};
 
@@ -16,10 +16,8 @@ fn get_my_processes(system : &mut sysinfo::System) -> String {
         my_vec.push(process.name().to_string());
         my_vec.push(format!("{:?}", process.cpu_usage()));
         my_vec.push(format!("{:?}", process.memory()));
-        // my_vec.push(format!("{:?}", process.status()));
     }
     let mut my_s = String::with_capacity(2048);
-    // my_s.push_str(&format!("{:^5}: {:^6}: {:^6}: {:^6}: {:^6}\n", "Pid", "Name", "Cpu(%)", "Memory(kb)",  "Status"));
     my_s.push_str(&format!("{:^5}: {:^6}: {:^6}: {:^6}\n", "Pid", "Name", "Cpu(%)", "Memory(kb)"));
     for x in (0..my_vec.len()).step_by(4) {
         my_s.push_str(&format!("{:^5}", &my_vec[x]));
@@ -29,8 +27,6 @@ fn get_my_processes(system : &mut sysinfo::System) -> String {
         my_s.push_str(&format!("{:^6}", &my_vec[x + 2])[0..6]);
         my_s.push_str(": ");
         my_s.push_str(&format!("{:^10}", &my_vec[x + 3]));
-        // my_s.push_str(": ");
-        // my_s.push_str(&format!("{:^6}", &my_vec[x + 4]));
         my_s.push_str("\n");
     }
     return my_s;
@@ -47,7 +43,7 @@ fn get_my_cpu_usage(system : &mut sysinfo::System) -> String {
     }
     let mut my_s = String::with_capacity(2048);
     for x in (0..my_vec.len()).step_by(1) {
-        my_s.push_str(&format!("{:^3}", &my_vec[x]));
+        my_s.push_str(&format!("[{:^2}]", x));
         my_s.push_str(" [");
         for i in (0..100).step_by(2) {
             if i < my_vec[x] as u8 {
@@ -57,7 +53,9 @@ fn get_my_cpu_usage(system : &mut sysinfo::System) -> String {
                 my_s.push_str(" ");
             }
         }
-        my_s.push_str("]\n");
+        my_s.push_str("] ");
+        my_s.push_str(&format!("{:^3}", &my_vec[x]));
+        my_s.push_str("\n");
     }
     return my_s;
 }
@@ -77,7 +75,6 @@ fn get_my_disks(system : &mut sysinfo::System) -> String {
     for disk in system.get_disks() {
         my_vec.push(disk.get_name().to_string_lossy().into_owned());
         my_vec.push(get_disk_type_string(disk.get_type()));
-        // my_vec.push(format!("{:?}", disk.get_file_system()));
         my_vec.push(format!("{:?}", disk.get_mount_point()));
         my_vec.push(format!("{:?}", disk.get_total_space() / 1000000000));
         my_vec.push(format!("{:?}", disk.get_available_space() / 1000000000));
@@ -95,12 +92,41 @@ fn get_my_disks(system : &mut sysinfo::System) -> String {
         my_s.push_str(&format!("{:^12}", &my_vec[x + 3]));
         my_s.push_str(": ");
         my_s.push_str(&format!("{:^12}", &my_vec[x + 4]));
-        // my_s.push_str(": ");
-        // my_s.push_str(&format!("{}", &my_vec[x + 5]));
         my_s.push_str("\n");
     }
     return my_s;
 }
+
+
+    // println!("MEMORY");
+    // println!("total memory: {} KiB", system.get_total_memory());
+    // println!("used memory : {} KiB", system.get_used_memory());
+    // println!("total swap  : {} KiB", system.get_total_swap());
+    // println!("used swap   : {} KiB", system.get_used_swap());
+
+fn get_my_memory(system : &mut sysinfo::System) -> String {
+    system.refresh_all();
+    let mut my_vec = Vec::new();
+
+    my_vec.push(format!("{}", system.get_total_memory() / 1049000));
+    my_vec.push(format!("{}", system.get_used_memory()/ 1049000));
+    my_vec.push(format!("{}", system.get_total_swap()/ 1049000));
+    my_vec.push(format!("{}", system.get_used_swap()/ 1049000));
+
+    let mut my_s = String::with_capacity(2048);
+
+    my_s.push_str(&format!("{:^5}: {:^5}: {:^5}: {:^5}\n", "Total", "Used", "Swap", "Used"));
+    my_s.push_str(&format!("{}(GiB)", &my_vec[0]));
+    my_s.push_str(": ");
+    my_s.push_str(&format!("{}(GiB)", &my_vec[1]));
+    my_s.push_str(": ");
+    my_s.push_str(&format!("{}(GiB)", &my_vec[2]));
+    my_s.push_str(": ");
+    my_s.push_str(&format!("{}(GiB)", &my_vec[3]));
+    my_s.push_str("\n");
+    return my_s;
+}
+
 
 fn my_loop(s: &mut Cursive) {
     let mut system = sysinfo::System::new_all();
@@ -109,12 +135,20 @@ fn my_loop(s: &mut Cursive) {
     let process_string = get_my_processes(&mut system);
     let cpu_string = get_my_cpu_usage(&mut system);
     let disk_string = get_my_disks(&mut system);
+    let memory_string = get_my_memory(&mut system);
 
     let process = Dialog::text(process_string).title("Running Processes");
     let cpu = Dialog::text(cpu_string).title("CPU Usage");
     let disks = Dialog::text(disk_string).title("Disks Info");
-
-    let  layout = LinearLayout::vertical().child(cpu).child(LinearLayout::horizontal().child(process).child(disks));
+    let memory = Dialog::text(memory_string).title("Memory Info");
+    
+    let layout = LinearLayout::vertical()
+        // .child(LinearLayout::horizontal()
+                .child(cpu)
+                .child(memory)
+        .child(LinearLayout::horizontal()
+            .child(process)
+            .child(disks));
     s.add_layer(layout);
 }
 
@@ -123,7 +157,6 @@ fn main() {
 
     let mut siv = cursive::default();
     siv.add_global_callback('q', |s| s.quit());
-    // siv.add_global_callback(' ', |s| my_loop(s));
 
 
     while siv.is_running() {
@@ -132,50 +165,10 @@ fn main() {
         siv.refresh();
         thread::sleep(time::Duration::from_millis(1000));
     }
-    
-
-    // siv.run();
-    // let mut system = sysinfo::System::new_all();
-    // let s = get_my_disks(&mut system);
-    // println!("{}", s);
-
-
-
-    // Now let's print every process' id and name:
-    // for (pid, proc_) in system.get_processes() {
-    //     println!("{}:{} => status: {:?}", pid, proc_.name(), proc_.status());
-    // }
-    
-    // // Then let's print the temperature of the different components:
-    // for component in system.get_components() {
-    //     println!("{:?}", component);
-    // }
-    
-    // // And then all disks' information:
-    // for disk in system.get_disks() {
-    //     println!("{:?}", disk);
-    // }
-    
-
-    
-    // println!("PROCESS");
-    // for (pid, process) in system.get_processes() {
-    //     println!("[{}] {} {:?} {}Kb", pid, process.name(), process.disk_usage(), process.memory());
-    // }
-
-    // println!("PROCESSOR");
-    // for processor in system.get_processors() {
-    //     println!("{}%", processor.get_cpu_usage());
-    // }
 
     // println!("NETWORK");
     // for (interface_name, data) in system.get_networks() {
     //     println!("{}: {}/{} B", interface_name, data.get_received(), data.get_transmitted());
     // }
 
-    // println!("MEMORY");
-    // println!("total memory: {} KiB", system.get_total_memory());
-    // println!("used memory : {} KiB", system.get_used_memory());
-    // println!("total swap  : {} KiB", system.get_total_swap());
-    // println!("used swap   : {} KiB", system.get_used_swap());
 }
